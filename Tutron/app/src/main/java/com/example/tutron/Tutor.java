d9f868e23b1ec999246e9b75892582dde3a8095f
Jason <jshum086@uottawa.ca>
2023-06-11 15:42:50 -0400

    made onClick functions for Tutor.java

--- Tutron/app/src/main/java/com/example/tutron/Tutor.java
+++ Tutron/app/src/main/java/com/example/tutron/Tutor.java
@@ -2,13 +2,39 @@ package com.example.tutron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Tutor extends AppCompatActivity {
+
+    Button backTutorBtn;
+    Button createTutorBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor);
+
+        backTutorBtn = findViewById(R.id.tutorBackButton);
+        backTutorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backIntent = new Intent(Tutor.this, GetTutorOrStudent.class);
+                startActivity(backIntent);
+                finish();
+            }
+        });
+
+        createTutorBtn = findViewById(R.id.tutorCreateButton);
+        createTutorBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent backIntent = new Intent(Tutor.this, MainActivity.this);
+                startActivity(backIntent);
+                finish();
+            }
+        });
     }
 }
