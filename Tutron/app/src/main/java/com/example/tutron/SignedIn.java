66e51a31f856641ca15a3e30cb4b91ea05171083
Jason <jshum086@uottawa.ca>
2023-06-11 16:42:26 -0400

    changes to activity

--- Tutron/app/src/main/java/com/example/tutron/SignedIn.java
+++ Tutron/app/src/main/java/com/example/tutron/SignedIn.java
@@ -25,7 +25,5 @@ public class SignedIn extends AppCompatActivity {
                 finish();
             }
         });
-
-
     }
 }
--- Tutron/app/src/main/java/com/example/tutron/View.java
+++
@@ -1,4 +0,0 @@
-package com.example.tutron;
-
-public class View {
-}
