0d718be376368655ef3a2d3072b1c92455312a69
Jason <jshum086@uottawa.ca>
2023-06-11 22:19:03 -0400

    modified tutor registration

--- Tutron/app/src/main/java/com/example/tutron/Tutor.java
+++ Tutron/app/src/main/java/com/example/tutron/Tutor.java
@@ -1,5 +1,6 @@
 package com.example.tutron;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,15 +11,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Tutor extends AppCompatActivity {
     EditText tutorUserName, firstName, lastName, educationLevel, emailAddress, password, language, description;
     Button backTutorBtn;
     Button createTutorBtn;
     FirebaseDatabase database;
-    DatabaseReference reference;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://seg-2105-group-project-f5fd7-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +59,7 @@ public class Tutor extends AppCompatActivity {
                 String emailTemp = emailAddress.getText().toString().trim();
                 String passwordTemp = password.getText().toString().trim();
                 String languageTemp = language.getText().toString().trim();
-                String descriptionTemp = language.getText().toString().trim();
+                String descriptionTemp = description.getText().toString().trim();
                 Boolean dataSaved = false;
 
                 if (!TextUtils.isEmpty(userNameTemp) && !TextUtils.isEmpty(firstNameTemp) && !TextUtils.isEmpty(lastNameTemp)
@@ -66,10 +70,19 @@ public class Tutor extends AppCompatActivity {
                     Toast.makeText(Tutor.this, "Failed. All fields must be filled in.", Toast.LENGTH_SHORT).show();
                 }
                 if(dataSaved){
-                    database = FirebaseDatabase.getInstance();
-                    reference = database.getReference("users");
-                    HelperClass helperClass = new HelperClass(userNameTemp, passwordTemp, "Tutor");
-                    reference.child(userNameTemp).setValue(helperClass);
+                    databaseReference.child("users").child(userNameTemp).child("first name").setValue(firstNameTemp);
+                    databaseReference.child("users").child(userNameTemp).child("last name").setValue(lastNameTemp);
+                    databaseReference.child("users").child(userNameTemp).child("education").setValue(educationTemp);
+                    databaseReference.child("users").child(userNameTemp).child("email").setValue(emailTemp);
+                    databaseReference.child("users").child(userNameTemp).child("password").setValue(passwordTemp);
+                    databaseReference.child("users").child(userNameTemp).child("language").setValue(languageTemp);
+                    databaseReference.child("users").child(userNameTemp).child("description").setValue(descriptionTemp);
+                    databaseReference.child("users").child(userNameTemp).child("user type").setValue("tutor");
+
+                    //database = FirebaseDatabase.getInstance();
+                    //reference = database.getReference("users");
+                    //HelperClass helperClass = new HelperClass(userNameTemp, passwordTemp, "Tutor");
+                    //reference.child(userNameTemp).setValue(helperClass);
 
                     Toast.makeText(Tutor.this, "Successful!", Toast.LENGTH_SHORT).show();
                     Intent register = new Intent(Tutor.this, MainActivity.class);
