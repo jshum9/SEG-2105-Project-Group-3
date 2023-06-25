f5376595c32dbcd1aa625cbaecf20b9951fbd356
Shawhin Niktash <shawhin.n@gmail.com>
2023-06-25 18:00:50 -0400

    Added UI test for administrator login

--- Tutron/app/src/androidTest/java/com/example/tutron/MainActivityTest.java
+++ Tutron/app/src/androidTest/java/com/example/tutron/MainActivityTest.java
@@ -18,6 +18,7 @@ import androidx.test.core.app.ActivityScenario;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
@@ -46,6 +47,18 @@ public class MainActivityTest {
         onView(withId(R.id.registerBtn)).perform(click());
 
         intended(hasComponent(GetTutorOrStudent.class.getName()));
+    }
+
+    @Test
+    public void testAdministratorSignIn(){
+        ActivityScenario.launch(MainActivity.class);
+
+        onView(withId(R.id.userEmail)).perform(typeText("Administrator"));
+        onView(withId(R.id.userPassword)).perform(typeText("password"));
+        onView(withId(R.id.loginBtn)).perform(click());
+
+        intended(hasComponent(Administrator.class.getName()));
+
     }
 }
 
