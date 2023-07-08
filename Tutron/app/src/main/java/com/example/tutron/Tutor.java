54e645440ba5192043eff324c181263e24898b11
Shawhin Niktash <shawhin.n@gmail.com>
2023-07-08 15:22:09 -0400

    Small update

--- Tutron/app/src/main/java/com/example/tutron/Tutor.java
+++ Tutron/app/src/main/java/com/example/tutron/Tutor.java
@@ -27,10 +27,10 @@ public class Tutor extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //generate a test complaint
-        String id = databaseReference.push().getKey();
-        Complaint a = new Complaint(id, "This is a test complaint. I don't like my Tutor, he sucks. Give me new one! I want my money BACK!!!! I got bad grade.... Bla bla..adfajdf;lajdfl;ajdfljal;kdjfal;sjdflajdfl;jasdl;kfjals;djf;alksdjfla;ksdjflkajsdl;fkjasld;jfal;ksdjfl;kajsdlf;kjasldk;fjalksdjflkajsdlfkjalksdjflak;sdjfl;ajsdfl;kajsdfl;kjasdlkfjalds;kjfal;sdjflajdfl;kajdslf;kjasdl;kfjal;sdjfl;ajsd;flakjdsfl;kajsd;lfkjasdl;kfjaldks;jfl;kasdjflasjf", "testStudentEmail@gmail.com", "testTutorEmail@gmail.com");
-        assert id != null;
-        databaseReference.child("Complaints").child(id).setValue(a);
+//        String id = databaseReference.push().getKey();
+//        Complaint a = new Complaint(id, "This is a test complaint. I don't like my Tutor, he sucks. Give me new one! I want my money BACK!!!! I got bad grade.... Bla bla..adfajdf;lajdfl;ajdfljal;kdjfal;sjdflajdfl;jasdl;kfjals;djf;alksdjfla;ksdjflkajsdl;fkjasld;jfal;ksdjfl;kajsdlf;kjasldk;fjalksdjflkajsdlfkjalksdjflak;sdjfl;ajsdfl;kajsdfl;kjasdlkfjalds;kjfal;sdjflajdfl;kajdslf;kjasdl;kfjal;sdjfl;ajsd;flakjdsfl;kajsd;lfkjasdl;kfjaldks;jfl;kasdjflasjf", "testStudentEmail@gmail.com", "testTutorEmail@gmail.com");
+//        assert id != null;
+//        databaseReference.child("Complaints").child(id).setValue(a);
 
 
 
